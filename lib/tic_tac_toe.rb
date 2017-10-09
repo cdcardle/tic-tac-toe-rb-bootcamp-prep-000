cfcd97bb751f90c9db65743e9be85eab0b0c651a
Chris Cardle <cdcardle@gmail.com>
2017-10-09 16:47:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,9 +63,9 @@ end
 def won?(board)
   WIN_COMBINATIONS.select do |win_com|
     if (board[win_com[0]] == "X" && board[win_com[1]] == "X" && board[win_com[2]] == "X")
-      return win_com && true
+      return win_com
     elsif (board[win_com[0]] == "O" && board[win_com[1]] == "O" && board[win_com[2]] == "O")
-      return win_com && true
+      return win_com
     elsif board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       return nil
     end
@@ -75,4 +75,14 @@ end
 
 def full?(board)
   board.all?{|i| (i == "O" || i == "X")}
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  elsif !full?(board) && !won?(board)
+    false
+  elsif won?(board)
+    false
+  end
 end
