342cb707d1504ed230e1146b7f2467263fbb4ac2
Chris Cardle <cdcardle@gmail.com>
2017-10-09 16:56:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,7 +76,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if (full?(board) = true && !won?(board))
+  if (full?(board) == true && !won?(board))
     true
   elsif (!full?(board) && !won?(board))
     false
