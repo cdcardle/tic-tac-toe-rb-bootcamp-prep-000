57ef867a284ba6742ae5401547fb1d6dbcfcdf8a
Chris Cardle <cdcardle@gmail.com>
2017-10-09 17:16:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, token="X")
+    move(board, index, "#{current_player}")
     display_board(board)
   else
     turn(board)
@@ -101,6 +101,7 @@ def play(board)
   end
   if won?(board)
     puts "Congrulations, #{winner} has won!"
+    
   elsif draw?(board)
     "Game has ended in a draw!"
   end
